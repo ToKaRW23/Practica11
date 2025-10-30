328603985e528e9ee68f9c1b3a1ab583d289ce26
ToKaRW23 <ananasik203@gmail.com>
2025-10-30 23:56:10 +0300

    feat: implement core game logic (guess, hints, attempts)

--- src/GuessTheNumber.java
+++ src/GuessTheNumber.java
@@ -1,15 +1,31 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Random;
+import java.util.Scanner;
+
 public class GuessTheNumber {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how OpenIDE suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        int target = random.nextInt(100) + 1;
+        int attempts = 0;
+        int guess;
+
+        System.out.println("Я загадал число от 1 до 100. Попробуй угадать!");
+
+        do {
+            System.out.print("Твой вариант: ");
+            guess = scanner.nextInt();
+            attempts++;
+
+            if (guess < target) {
+                System.out.println("Больше!");
+            } else if (guess > target) {
+                System.out.println("Меньше!");
+            } else {
+                System.out.printf("Угадал за %d попыток!\n", attempts);
+            }
+        } while (guess != target);
+
+        System.out.println("Игра окончена.");
     }
 }
